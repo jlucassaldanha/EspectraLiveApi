2ebee102708178c31fc15454375ad69cd8c31a7f
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-03-13 16:24:43 -0300

    Criando arquivos iniciais e models

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+namespace SpectraLiveApi.Models;
+
+public class User
+{
+	public Guid Id { get; private set; }
+	public string AccessToken { get; private set; }
+	public string RefreshToken { get; private set; }
+	public string ExpiresIn { get; private set; }
+	public string TwitchId { get; private set; }
+	public string Login { get; private set; }
+	public string DisplayName { get; private set; }
+	public string Email { get; private set; }
+	public string ProfileImgUrl { get; private set; }
+
+	public User(string accessToken, string refreshToken, string expiresIn, string twitchId, string login, string displayName, string email, string profileImgUrl)
+	{
+		AccessToken = accessToken;
+		RefreshToken = refreshToken;
+		ExpiresIn = expiresIn;
+		TwitchId = twitchId;
+		Login = login;
+		DisplayName = displayName;
+		Email = email;
+		ProfileImgUrl = profileImgUrl;
+
+		Id = Guid.NewGuid();
+	}
+}
