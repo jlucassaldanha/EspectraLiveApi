069b8caed6a06819e39e6436dd3bb188cf1f8a79
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-03-24 11:09:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<TwitchService>();
 builder.Services.AddScoped<UnviewsService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
